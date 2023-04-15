4be20a168d17c7a8bf1eea2c6020d1f426953d03
jorgeProgramaEn5 <116350790+jorgeProgramaEn5@users.noreply.github.com>
2023-04-15 12:18:08 -0400

    add index + stilo + js

---
+++ funciones.js
@@ -0,0 +1,10 @@
+//------ Menu Responsive------//
+//con el classList se llama al estilo css open, y con el add se agrega
+function mostrar() {
+    document.getElementById("sliding_menu").classList.add('open'); 
+  }     
+//con el classList se llama al estilo css open, y con el remve se remueve
+function ocultar() {
+    document.getElementById("sliding_menu").classList.remove('open'); 
+  } 
+//------Fin Menu Responsive------//
